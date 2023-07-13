3a93bd8a4c7ddb941907338ff7c868f2b4ad1a46
Anton Parshikov <tosha1703@gmail.com>
2023-07-13 16:30:06 +0300

    add files

---
+++ catalog/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from catalog.views import home, contacts
+from . import views
+
+urlpatterns = [
+    path('', home),
+    path('contacts/', views.contacts, name='contacts')
+
+]
