42d19f94b04a03520786f1447efba757b0c6146c
Anton Parshikov <tosha1703@gmail.com>
2023-08-04 17:20:35 +0300

    add fix

--- config/settings.py
+++ config/settings.py
@@ -141,6 +141,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tosha1703@gmail.com'
-EMAIL_HOST_PASSWORD = '**********'
+EMAIL_HOST_PASSWORD = '***********'
 
 
